810dcb27ae7162e6501b1ec597546b8818776e5a
micaelgallego <micael.gallego@gmail.com>
2019-09-26 17:31:50 +0200

    Ejer14

---
+++ Ejers/src/functions/joingGetTogether.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+
+module.exports.handler = async event => {
+
+    console.log('Body: '+event.body);
+    console.log('Topic name: '+process.env.getTogethersTopicName)
+
+    const user = JSON.parse(event.body);    
+
+    const orderId = 'sssss';
+
+    user.orderId = orderId;
+
+    const sns = new AWS.SNS();
+
+    
+
+    const params = {
+        Message: JSON.stringify(user),
+        TopicArn: process.env.getTogethersTopic
+    }
+
+    await sns.publish(params).promise();
+    
+    const res = {
+        statusCode: 200,
+        body: orderId
+    };
+
+    return res;
+};
