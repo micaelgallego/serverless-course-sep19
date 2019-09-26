cca111db7d9f4c696f9acaa5e9f5024e1766c2a2
micaelgallego <micael.gallego@gmail.com>
2019-09-26 12:00:16 +0200

    Ejer6

--- Ejers/tests/helloWorld.test.js
+++ Ejers/tests/helloWorld.test.js
@@ -5,8 +5,7 @@ describe(`When we invoke the GET /helloWorld endpoint`, () => {
     test(`Should return the right greeting`, async () => {
 
         const response = await when.we_invoke_helloWorld("Manolito");
-        
-        response.body = JSON.parse(response.body);
+
         expect(response.statusCode).toBe(200);
         expect(response.body).toBe("Hello Manolito");
     });
--- Ejers/tests/steps/when.js
+++ Ejers/tests/steps/when.js
@@ -1,9 +1,34 @@
+//var Promise = this.Promise || require('promise');
+var http = require('superagent-promise')(require('superagent'), Promise);
+
 module.exports.we_invoke_helloWorld = (name) => {
     const event = { pathParameters: { name: name } };
-    return viaHandler("helloWorld", event);
+
+    const testMode = process.env.TEST_MODE;
+
+    if(testMode === 'handler'){
+        return viaHandler("helloWorld", event);    
+    } if(testMode === 'http'){
+        return viaHttp('helloWorld', name);
+    } else {
+        console.error(`Unrecognized TEST_MODE ${testMode}`);
+    }
 }
 
 function viaHandler(funcName, event){
     const handler = require(`../../src/functions/${funcName}`);
+    console.log(handler);
     return handler.handler(event);
+}
+
+async function viaHttp(funcName, name){
+
+    const url = `https://v43r7coe0d.execute-api.eu-west-1.amazonaws.com/dev/api/${funcName}/`;
+    
+    const response = await http('GET', url + name);
+
+    return {
+        statusCode: response.status,
+        body: response.body
+    }
 }
